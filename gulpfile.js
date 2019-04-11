0f06e736a32ec7bcdc7ab18f5df4a057f411a357
kimhun <bdrk995@gmail.com>
2019-04-11 16:39:17 +0700

    1st commit

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const { src, dest} = require("gulp");
+const scss = require("gulp-sass");
+const minifycss = require("gulp-minify-css");
+const browserSync = require("browser-sync").create();
+
+
+// Static server
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+}
+function css() {
+    return src("app/scss/styles.scss")
+      .pipe(scss())
+      .pipe(minifycss())
+      .pipe(dest("app/css"))
+      .pipe(browsersync.stream());
+  }
+  
+
+exports.css = css;
+exports.browsersync = browsersync;
+exports.default = (css , browsersync);
